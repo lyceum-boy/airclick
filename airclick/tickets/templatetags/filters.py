7ba25e93e687d247852e1bfc46f992ca03fdbc5d
Илья Аносов <74177997+lyceum-boy@users.noreply.github.com>
2024-12-26 02:37:57 +0300

    Add detailed pages

---
+++ airclick/tickets/templatetags/filters.py
@@ -0,0 +1,21 @@
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def format_price(value):
+    try:
+        return f"{value:,} ₽".replace(",", " ")
+    except (ValueError, TypeError):
+        return value
+
+
+@register.filter
+def format_duration(value):
+    try:
+        hours = value // 60
+        minutes = value % 60
+        return f"{hours} ч. {minutes} мин." if hours else f"{minutes} мин."
+    except (ValueError, TypeError):
+        return value
